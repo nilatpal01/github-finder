59d3440836c38abad1c0ad3300907ff2c2c4aee7
nilatpal01 <dasn0101@gmail.com>
2022-07-30 17:08:37 +0530

    initial commit

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,30 @@
+import {createContext, useReducer} from 'react';
+import GithubReducer from './GithubReducer';
+
+const GithubContext = createContext();
+
+export const GithubProvider = ({children}) => {
+
+    const initialState = {
+        users: [],
+        user: {},
+        repos:[],
+        loading: false
+    }
+
+    const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+    return <GithubContext.Provider 
+        value={{
+            // users:state.users,
+            // loading:state.loading,
+            // user:state.user,
+            // repos:state.repos,
+            ...state,
+            dispatch,
+        }}>
+        {children}
+    </GithubContext.Provider>
+}
+
+export default GithubContext;
